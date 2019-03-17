becf2271d7616621db5618e60d59872bccbd1b7c
shantidev <shanti.devv@gmail.com>
2019-03-18 01:38:24 +0500

    Delete needless

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,21 +6,18 @@ import TextInput from '../../ui/TextInput';
 
 const Form = props => {
   const { form } = props;
-  //const input = useRef(null);
 
   return (
     <UI.FormContainer>
       <UI.FormRow>
         <TextInput
-          // ref={input}
           name={form.fields.name.name}
           value={form.fields.name.value}
           change={form.onChange}
           label="Ваше имя"
-          // invalid={form.name}
         />
       </UI.FormRow>
-      <div onClick={form.onSubmit}>BUTTON</div>
+      <button onClick={form.onSubmit}>BUTTON</button>
     </UI.FormContainer>
   )
 };
